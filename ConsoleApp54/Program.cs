f609617e6efbeb6101fdab9192e07f2463894746
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-17 15:50:00 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp54/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            do
+            {
+                try
+                {
+                    Console.Write("a= ");
+                    a = Convert.ToDouble(Console.ReadLine());
+                    if  (a <= 0) {Console.WriteLine("Side must be greater than 0.");
+                        continue; }
+                
+                    b = a * a;
+                    c = 4 * a;
+                    Console.WriteLine("Area is equal to {0}, perimeter is equal to {1}", b, c);
+                break;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Incorrect input data format");
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine("Error message: {0}", ex.Message);
+                }
+            } while (1 == 1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp54/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("ConsoleApp54")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp54")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("04e752ee-4fc2-4213-a4be-f77dcfefa5b7")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
